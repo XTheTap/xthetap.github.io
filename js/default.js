1dc39cac8b0e7ab2fbea03b249f6f53b7d1b6d92
illia <illia9842@gmail.com>
2025-01-10 09:58:10 +0300

    Offline worker fix

--- js/default.js
+++ js/default.js
@@ -3,19 +3,3 @@ if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('/sw.js');
     });
 }
-
-var CACHE_NAME = 'my-offline-cache';
-var urlsToCache = [
-  '/',
-  '/css/default.css',
-  '/js/default.js'
-];
-
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
